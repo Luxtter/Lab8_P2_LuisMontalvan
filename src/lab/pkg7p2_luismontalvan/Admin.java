8f57754f69ad21afe6d36aba37952ca49266b349
Luxtter <115840511+Luxtter@users.noreply.github.com>
2023-06-09 14:00:57 -0600

    Admin

---
+++ src/lab/pkg7p2_luismontalvan/Admin.java
@@ -0,0 +1,43 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package lab.pkg7p2_luismontalvan;
+
+/**
+ *
+ * @author luism
+ */
+public class Admin {
+    private String usuario,contrasena;
+
+    public Admin() {
+    }
+
+    public Admin(String usuario, String contrasena) {
+        this.usuario = usuario;
+        this.contrasena = contrasena;
+    }
+
+    public String getUsuario() {
+        return usuario;
+    }
+
+    public void setUsuario(String usuario) {
+        this.usuario = usuario;
+    }
+
+    public String getContrasena() {
+        return contrasena;
+    }
+
+    public void setContrasena(String contrasena) {
+        this.contrasena = contrasena;
+    }
+
+    @Override
+    public String toString() {
+        return "Admin{" + "usuario=" + usuario + ", contrasena=" + contrasena + '}';
+    }
+    
+}
