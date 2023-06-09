216b1e200dc5d06a69b5d4f0abbc9b88e38bafc8
Luxtter <115840511+Luxtter@users.noreply.github.com>
2023-06-09 13:45:45 -0600

    Clase artista

---
+++ src/lab/pkg7p2_luismontalvan/Artista.java
@@ -0,0 +1,64 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package lab.pkg7p2_luismontalvan;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author luism
+ */
+public class Artista {
+    protected String usuario,contrasena,nombre,genero;
+    protected ArrayList<Cancion> canciones = new ArrayList();
+    
+    public Artista() {
+    }
+
+    public Artista(String usuario, String contrasena, String nombre, String genero) {
+        this.usuario = usuario;
+        this.contrasena = contrasena;
+        this.nombre = nombre;
+        this.genero = genero;
+    }
+
+    public String getGenero() {
+        return genero;
+    }
+
+    public void setGenero(String genero) {
+        this.genero = genero;
+    }
+
+    public String getUsuario() {
+        return usuario;
+    }
+
+    public void setUsuario(String usuario) {
+        this.usuario = usuario;
+    }
+
+    public String getContrasena() {
+        return contrasena;
+    }
+
+    public void setContrasena(String contrasena) {
+        this.contrasena = contrasena;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    @Override
+    public String toString() {
+        return "Artista{" + "usuario=" + usuario + ", contrasena=" + contrasena + ", nombre=" + nombre + '}';
+    }
+    
+}
