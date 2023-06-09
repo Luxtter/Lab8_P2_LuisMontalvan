1b867473375713a7b0a7dbbc0bca0702d720b748
Luxtter <115840511+Luxtter@users.noreply.github.com>
2023-06-09 14:06:47 -0600

    Cancion

--- src/lab/pkg7p2_luismontalvan/Cancion.java
+++ src/lab/pkg7p2_luismontalvan/Cancion.java
@@ -9,5 +9,36 @@ package lab.pkg7p2_luismontalvan;
  * @author luism
  */
 public class Cancion {
+    private String nombre;
+    private int duracion;
+
+    public Cancion() {
+    }
+
+    public Cancion(String nombre, int duracion) {
+        this.nombre = nombre;
+        this.duracion = duracion;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getDuracion() {
+        return duracion;
+    }
+
+    public void setDuracion(int duracion) {
+        this.duracion = duracion;
+    }
+
+    @Override
+    public String toString() {
+        return "Cancion{" + "nombre=" + nombre + ", duracion=" + duracion + '}';
+    }
     
 }
