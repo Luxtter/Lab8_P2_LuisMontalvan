833a26ac952d99fc47936b5b38fb52dc49d8a6f2
Luxtter <115840511+Luxtter@users.noreply.github.com>
2023-06-09 14:30:18 -0600

    Inicio de sesion listo

--- src/lab/pkg7p2_luismontalvan/Principal.java
+++ src/lab/pkg7p2_luismontalvan/Principal.java
@@ -4,6 +4,9 @@
  */
 package lab.pkg7p2_luismontalvan;
 
+import java.util.ArrayList;
+import javax.swing.JOptionPane;
+
 /**
  *
  * @author luism
@@ -15,6 +18,7 @@ public class Principal extends javax.swing.JFrame {
      */
     public Principal() {
         initComponents();
+        usuarios.add(new Admin("enuila","wynaut1325"));
     }
 
     /**
@@ -26,21 +30,132 @@ public class Principal extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
+        InicioSesion = new javax.swing.JPanel();
+        jLabel1 = new javax.swing.JLabel();
+        jLabel2 = new javax.swing.JLabel();
+        jtfUsuario = new javax.swing.JTextField();
+        jpfContrasena = new javax.swing.JPasswordField();
+        jLabel3 = new javax.swing.JLabel();
+        jbtnIniciarSesion = new javax.swing.JButton();
+
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+
+        jLabel1.setFont(new java.awt.Font("Georgia", 0, 18)); // NOI18N
+        jLabel1.setText("Inicio de Sesion");
+
+        jLabel2.setFont(new java.awt.Font("Georgia", 0, 12)); // NOI18N
+        jLabel2.setText("Usuario");
+
+        jpfContrasena.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jpfContrasenaActionPerformed(evt);
+            }
+        });
+
+        jLabel3.setFont(new java.awt.Font("Georgia", 0, 12)); // NOI18N
+        jLabel3.setText("Contraseña");
+
+        jbtnIniciarSesion.setText("Iniciar Sesion");
+        jbtnIniciarSesion.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseClicked(java.awt.event.MouseEvent evt) {
+                jbtnIniciarSesionMouseClicked(evt);
+            }
+        });
+
+        javax.swing.GroupLayout InicioSesionLayout = new javax.swing.GroupLayout(InicioSesion);
+        InicioSesion.setLayout(InicioSesionLayout);
+        InicioSesionLayout.setHorizontalGroup(
+            InicioSesionLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(InicioSesionLayout.createSequentialGroup()
+                .addGap(77, 77, 77)
+                .addGroup(InicioSesionLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(jLabel3)
+                    .addComponent(jLabel2))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addGroup(InicioSesionLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                    .addComponent(jpfContrasena, javax.swing.GroupLayout.DEFAULT_SIZE, 137, Short.MAX_VALUE)
+                    .addComponent(jtfUsuario))
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, InicioSesionLayout.createSequentialGroup()
+                .addGap(0, 0, Short.MAX_VALUE)
+                .addComponent(jLabel1)
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+            .addGroup(InicioSesionLayout.createSequentialGroup()
+                .addGap(237, 237, 237)
+                .addComponent(jbtnIniciarSesion)
+                .addContainerGap(262, Short.MAX_VALUE))
+        );
+        InicioSesionLayout.setVerticalGroup(
+            InicioSesionLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(InicioSesionLayout.createSequentialGroup()
+                .addGap(48, 48, 48)
+                .addComponent(jLabel1)
+                .addGap(60, 60, 60)
+                .addGroup(InicioSesionLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(jLabel2)
+                    .addComponent(jtfUsuario, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addGap(44, 44, 44)
+                .addGroup(InicioSesionLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(jpfContrasena, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(jLabel3))
+                .addGap(87, 87, 87)
+                .addComponent(jbtnIniciarSesion)
+                .addContainerGap(177, Short.MAX_VALUE))
+        );
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 600, Short.MAX_VALUE)
+            .addComponent(InicioSesion, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 500, Short.MAX_VALUE)
+            .addComponent(InicioSesion, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
         );
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
+
+    private void jpfContrasenaActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jpfContrasenaActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_jpfContrasenaActionPerformed
+
+    private void jbtnIniciarSesionMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_jbtnIniciarSesionMouseClicked
+        for (int i = 0; i < usuarios.size(); i++) {
+            if (usuarios.get(i) instanceof Admin){
+                if (((Admin)usuarios.get(i)).getUsuario().equals(jtfUsuario.getText())) {
+                    if (((Admin)usuarios.get(i)).getContrasena().equals(jpfContrasena.getText())) {
+                        InicioSesion.setVisible(false);
+                    }else{
+                        JOptionPane.showMessageDialog(InicioSesion, "Usuario o contraseña incorrecta");
+                    }
+                }else{
+                    JOptionPane.showMessageDialog(InicioSesion, "Usuario o contraseña incorrecta");
+                }
+            }else if(usuarios.get(i) instanceof Solista){
+                if (((Solista)usuarios.get(i)).getUsuario().equals(jtfUsuario.getText())) {
+                    if (((Solista)usuarios.get(i)).getContrasena().equals(jpfContrasena.getText())) {
+                        InicioSesion.setVisible(false);
+                    }else{
+                        JOptionPane.showMessageDialog(InicioSesion, "Usuario o contraseña incorrecta");
+                    }
+                }else{
+                    JOptionPane.showMessageDialog(InicioSesion, "Usuario o contraseña incorrecta");
+                }
+            }else{
+                if (((Banda)usuarios.get(i)).getUsuario().equals(jtfUsuario.getText())) {
+                    if (((Banda)usuarios.get(i)).getContrasena().equals(jpfContrasena.getText())) {
+                        InicioSesion.setVisible(false);
+                    }else{
+                        JOptionPane.showMessageDialog(InicioSesion, "Usuario o contraseña incorrecta");
+                    }
+                }else{
+                    JOptionPane.showMessageDialog(InicioSesion, "Usuario o contraseña incorrecta");
+                }
+            }
+        }
+    }//GEN-LAST:event_jbtnIniciarSesionMouseClicked
 
     /**
      * @param args the command line arguments
@@ -78,5 +193,13 @@ public class Principal extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JPanel InicioSesion;
+    private javax.swing.JLabel jLabel1;
+    private javax.swing.JLabel jLabel2;
+    private javax.swing.JLabel jLabel3;
+    private javax.swing.JButton jbtnIniciarSesion;
+    private javax.swing.JPasswordField jpfContrasena;
+    private javax.swing.JTextField jtfUsuario;
     // End of variables declaration//GEN-END:variables
+    ArrayList usuarios = new ArrayList();
 }
